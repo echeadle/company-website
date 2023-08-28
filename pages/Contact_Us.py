c8de908ad6567f6972eb96194eb8144ab9dc9552
echeadle <echeadle@gmail.com>
2023-08-27 21:25:25 -0600

    Day 23 Send emails using form and add __pycache__ to .gitignore

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -4,11 +4,15 @@ st.header("Contact Us")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your Email address")
-    message = st.text_area("Message")
-    message = message + "\n\n" + user_email
+    raw_message = st.text_area("Message")
+    message = f"""\ 
+Subject: New email from {user_email}
+
+From: {user_email} 
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        st.write(f"Thank you for your message. We will get back to you at {user_email} shortly.")
-        st.write(f"Your message was: {message}")
         send_email(message)
+        st.info("Your Email was sent successfully")
 
